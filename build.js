2e070e97171fbe78468d77eb18800a0aa25ed881
spider_under_your_bed <71126384+spiderlord202@users.noreply.github.com>
2023-05-18 11:01:45 +1200

    E

--- build.js
+++ build.js
@@ -1,12 +1,10 @@
-//const readline = require('readline');
-//const { spawn } = require('child_process');
+
 import fs from "fs";
 import jsonpath from "jsonpath";
 import naturalCompare from "string-natural-compare";
 import inquirer from "inquirer";
 import jsonfile from "jsonfile";
-//import vm2  from 'vm2';
-//C:\Users\DuttR\unofficalunityapiassistportal\build.js
+//current_obj[selected[selected.length - 1]]
 
 const logStream = fs.createWriteStream("my-log-file.txt", { flags: "a" });
 
@@ -236,41 +234,47 @@ const readFile = new Promise((resolve, reject) => {
           }
         }
         resolve(newRes);
-
-        //console.log(exportres)
       } catch (error) {
         console.log(error);
       }
     }
   });
-  //console.log(exportres)
-  //return exportres;
 });
 
-let selectedText = (selection) => {
-  return `[SELECTED (check to unselect): ${selection}]`;
-};
-``;
-////node C:\Users\DuttR\unofficalunityapiassistportal\build.js
-let iteration = 0;
-let selections = 0;
-let purelyaddedselect = 0;
-//let SelectedSafekeepNew = [];
 
-let path = [];
+let currentkeys = [];
+//let selectedAllin = false;
+let selectionPath = "";
+let selected = [];
+let selectedObj = {}
+
+let allInvoked = {}
+let selectionInvoked = {}
+let veiwInvoke = {}
 
 readFile.then((seedata) => {
-  function objselect(selected, obj) {
+  function objselect(obj) {
     return new Promise(function (resolve, reject) {
-      let currentkeys = [];
-      ///for (Object.keys(obj))
+      if (Object.keys(obj).length == 0) reject();
+      currentkeys = [];
       for (let i = 0; i < Object.keys(obj).length; i++) {
+        //if (selectedAllin == false){
+      //for (let l = 0; l < 2; l++) {
         if (selected.indexOf(Object.keys(obj)[i]) > -1) {
+         //- selectedObj[Object.keys(obj)[i]] = {}         
           currentkeys.push("selected: " + Object.keys(obj)[i]);
         } else {
+          //delete selectedObj[Object.keys(obj)[i]]
           currentkeys.push(Object.keys(obj)[i]);
         }
-      }
+     // }
+   //   } else {
+       // logStream.write("making all children selected")
+     //   currentkeys.push("selected: " + Object.keys(obj)[i]);
+      //}
+    }
+  //  selectedAllin = false;
+   // logStream.write("ended selection "+selectedAllin+"\n")
 
       let user = inquirer
         .prompt([
@@ -283,31 +287,32 @@ readFile.then((seedata) => {
         ])
         .then((answers) => {
           let awnser = answers.selectedItem[0];
-          // logStream.write(Object.keys(obj).indexOf(awnser.slice(0, 'selected: '.length))+"\n");
-          //node C:\Users\DuttR\unofficalunityapiassistportal\build.js
           if (
-            Object.keys(obj).indexOf(awnser.slice(0, "selected: ".length)) > -1
+            Object.keys(obj).indexOf(awnser.substring("selected: ".length, awnser.length)) > -1
           ) {
-            awnser = awnser.slice(0, "selected: ".length).trim();
-            logStream.write("awnser1: " + awnser + "\n");
+            awnser = awnser.substring("selected: ".length, awnser.length);
           }
+          selectionPath = awnser;
+
+         // for (let l = 0; l < 2; l++) {
           if (selected.indexOf(awnser) > -1) {
-            //logStream.write("awnser: "+awnser+"\n");
-            logStream.write("status: " + selected.indexOf(awnser) + "\n");
             selected.splice(selected.indexOf(awnser), 1);
-            logStream.write(selected.indexOf(awnser) + "\n");
-            //node C:\Users\DuttR\unofficalunityapiassistportal\build.js
           } else {
             selected.push(awnser);
-            logStream.write(awnser + "\n");
-            //selected.splice(selected.indexOf(answers.selectedItem[0]), 1);
           }
+          for (let i = 0; i < Object.keys(obj).length; i++) {
+            if (selected.indexOf(Object.keys(obj)[i]) > -1) {
+          selectedObj[Object.keys(obj)[i]] = {}         
+        } else {
+          delete selectedObj[Object.keys(obj)[i]]
+        }
+          }
+       // }
           resolve(); // Resolve the promise here
         });
     });
   }
 
-  let selected = [];
   let current_obj = seedata;
   async function Run() {
     while (current_obj instanceof Object) {
@@ -315,8 +320,6 @@ readFile.then((seedata) => {
         function resolution() {
           resolve();
         }
-
-        logStream.write("promise\n");
         let action = inquirer
           .prompt([
             {
@@ -327,220 +330,37 @@ readFile.then((seedata) => {
             },
           ])
           .then((answers) => {
-            logStream.write(answers.action + "\n");
+            fs.writeFile("totalstatus.json", JSON.stringify(selectedObj), function(error) {})
+            //logStream.write("selected: "+selected+"\n")
             if (answers.action == "Select") {
-              objselect(selected, current_obj).then(() => {
-                //logStream.write("before resolution select\n");
+                fs.writeFile("selectstatus.json", JSON.stringify(selectedObj), function(error) {})
+                selectionInvoked = selectedObj;
+            //  logStream.write("selected before select: "+selected+"\n")
+      //        if (selected.indexOf(selectionPath) > -1){
+        //        selectedAllin = true;
+          //      logStream.write("started selection "+selectedAllin+" : "+selectionPath+" : "+selected+"\n")
+            //} 
+          //  for (let l = 0; l < 2; l++) {
+              objselect(current_obj).then(() => {
                 resolution();
-              });
+              })
+            //}
             } else if (answers.action == "view") {
-              logStream.write(
-                current_obj[selected[selected.length - 1]] + "\n"
-              );
-              current_obj = current_obj[selected[selected.length - 1]];
+              fs.writeFile("statusfile.json", JSON.stringify(selectedObj), function(error) {})
+              veiwInvoke = selectedObj;
+              //logStream.write("selected before veiw: "+selected+"\n")
+              current_obj = current_obj[selectionPath]
+              //selectedAllin = true;
+             // logStream.write(selected.indexOf(selectionPath)+" selected: "+selected+"\n")
               resolution();
             }
+            //logStream.write(selected.indexOf(selectionPath)+" selected: "+selected+"\n")
           });
-      }).then(() => {
-        //logStream.write("finished iteration\n");
-      });
+      })
+      //.then(() => {
+      //});
     }
   }
 
   Run();
 });
-/*
-
-
-  readFile.then((seedata) => {
-  function objselect(selected, obj) {
-    return new Promise(function (resolve, reject) {
-      let currentkeys = Object.keys(obj);
-      let user = inquirer.prompt([
-        {
-          type: "checkbox",
-          name: "selectedItem",
-          message: "Select an item:",
-          choices: obj.keys,
-        },
-      ]).then((answers) => {
-        if (selected.indexOf(answers.selectedItem[0]) > -1) {
-          selected.push(answers.selectedItem[0]);
-        } else {
-          selected.splice(selected.indexOf(answers.selectedItem[0]), 1);
-        }
-        resolve(); // Resolve the promise when the selection is done
-      });
-    });
-  }
-
-  let selected = [];
-  let current_obj = seedata;
-  async function Run() {
-    while (current_obj instanceof Object) {
-      let action = await inquirer.prompt([
-        {
-          type: "list",
-          name: "action",
-          message: "Select an action:",
-          choices: ["Select", "view", "Select and view", "Back", "Quit"],
-        },
-      ]).then(async (answers) => {
-        if (answers.action == "select") {
-          await objselect(selected, current_obj); // Wait for objselect promise to resolve
-        } else if (answers.action == "view") {
-          current_obj = current_obj[selected[0]];
-        }
-      });
-    }
-  }
-
-  Run();
-});
-
-      for key in current_obj:
-        if key in selected:
-            print("selected: ", key)
-        else:
-            print(key)
-
-    action = input('what do you want to do: ')
-    if action == "select":
-        select_key(selected, current_obj)
-    elif action == "view":
-        current_obj = current_obj[selected[0]]
-        */
-
-/*
-
-selected = []
-
-current_obj = OBJECT
-while isinstance(current_obj, dict):
-    for key in current_obj:
-        if key in selected:
-            print("selected: ", key)
-        else:
-            print(key)
-
-    action = input('what do you want to do: ')
-    if action == "select":
-        select_key(selected, current_obj)
-    elif action == "view":
-        current_obj = current_obj[selected[0]]
-        */
-//			//node C:\Users\DuttR\unofficalunityapiassistportal\build.js
-/*
-  })
-  .catch((error) => {
-    console.log("An error occurred:", error);
-  });
-  
-}
-objselect(Object.keys(seedata))
-})
-*/
-//jsonfile.writeFile(file, obj, function (err) {
-//    if (err) console.error(err)
-//})
-//  logStream.write(`${Object.keys(keys)}\n`);
-
-/*
-rl.question('Enter the build path: ', (buildPath) => {
-
-        fs.writeFile('snippit_build.json', JSON.stringify(newRes, null, 2), (err) => {
-          if (err) throw err;
-          console.log('The file has been saved!');
-        });
-});
-*/
-
-/*
-  const child = spawn('node', [buildPath]);
-
-  child.stdout.on('data', (data) => {
-    console.log(`stdout: ${data}`);
-  });
-
-  child.stderr.on('data', (data) => {
-    console.error(`stderr: ${data}`);
-  });
-
-  child.on('close', (code) => {
-    console.log(`child process exited with code ${code}`);
-    rl.close();
-  });
-  */
-/*
-     let i = 0;
-
-    let pathNum = ['','[path[0]]','[path[0]][path[1]]','[path[0]][path[1]][path[2]]','[path[0]][path[1]][path[2]][path[3]]','[path[0]][path[1]][path[2]][path[3]][path[4]]'][iteration]
-		let code = ` 
-		try {
-    if (answers.action === "view"){
-			if (Object.keys(answers.selectedItem).length > 0 && Object.keys(${"seedata"+pathNum+"[answers.selectedItem]"}).length > 0){ 
-		objselect(Object.keys(${"seedata"+pathNum+"[answers.selectedItem]"}))
-		path.push(answers.selectedItem[i])
-			} else {
-        objselect(Object.keys(seedata${pathNum}));
-        //? '[0]' : '' || ''
-        iteration -= 1
-      }
-		 } else {
-      jsonfile.readFile("Selecteditems.json")
-      .then(obj => {
-      let workingObj = obj;
-    //  if (!workingObj.hasOwnProperty("SelectionArr")){
-      //  workingObj.SelectionArr = []
-      //}
-     // logStream.write(answers.selectedItem[i]) 
-			objselect(Object.keys(seedata${pathNum}).map(
-				(item) => 
-				{
-					//logStream.write("ITEM: " + item) 
-					//workingObj.build = []
-					
-					//workingObj.build.push(item)
-				//	jsonfile.writeFile("Selecteditems.json", workingObj, function (err) {
-					//	if (err) console.error(err)
-					//})
-          if (item == answers.selectedItem[i] && workingObj.SelectionArr.indexOf(item) > -1)
-						  {
-                workingObj.SelectionArr.splice(workingObj.SelectionArr.indexOf(item))
-                workingObj.thisRan = {}
-                jsonfile.writeFile("Selecteditems.json", workingObj, function (err) {
-                  if (err) console.error(err)
-                })
-								return item;
-              }
-          else if (item == answers.selectedItem[i]) 
-          {
-                workingObj.SelectionArr.push(item)
-                jsonfile.writeFile("Selecteditems.json", workingObj, function (err) {
-                  if (err) console.error(err)
-                })
-               // SelectedSafekeepNew.push(item)
-                  return selectedText(item);
-            } else if (workingObj.SelectionArr.indexOf(item) != -1){
-              return selectedText(item);
-            } else {  
-                return item; 
-            }
-
-					}
-						));
-			iteration -= 1
-          })
-          .catch(error => console.error(error))
-  		}
-		} catch (error){
-			logStream.write("ERROR: " + error) 
-		//	workingObj.error = "Error: "+error
-		//	jsonfile.writeFile("Selecteditems.json", workingObj, function (err) {
-			//	if (err) console.error(err)
-			//})
-		}
-			`
-		eval(code);
-    iteration += 1;
-    */
