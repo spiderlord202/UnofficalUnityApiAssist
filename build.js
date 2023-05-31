8d296fb5b1e72be7ecf8ac4a33fadd765e9a8bf0
spider_under_your_bed <71126384+SpiderUnderUrBed@users.noreply.github.com>
2023-06-01 11:56:31 +1200

    Back to normal

--- build.js
+++ build.js
@@ -2,200 +2,130 @@ import fs from "fs";
 import jsonpath from "jsonpath";
 import naturalCompare from "string-natural-compare";
 import inquirer from "inquirer";
-// @ts-ignore
-// @ts-ignore
 import jsonfile from "jsonfile";
-// @ts-ignore
-// @ts-ignore
 import { json } from "stream/consumers";
-// @ts-ignore
-// @ts-ignore
 import path from "path";
 //var treeify = require('treeify');
-import treeify from "treeify";
+import treeify from "treeify"
 //current_obj[selected[selected.length - 1]]
 
 const logStream = fs.createWriteStream("my-log-file.txt", { flags: "a" });
 
-// @ts-ignore
-// @ts-ignore
 let layers = [];
 
 logStream.write("created\n");
 
-// @ts-ignore
-// @ts-ignore
+
 function CreatePrefEnd(prestr) {
-  // console.log(str)
-  //if (prestr == null) return ["nothing"];
-  let str = "Word" + prestr;
+ // console.log(str)
+ //if (prestr == null) return ["nothing"];
+ let str = "Word"+prestr
   const words = str.match(/[A-Z][a-z]+/g);
   try {
-    const sorted = words.sort(naturalCompare);
+  const sorted = words.sort(naturalCompare);
+  
+  const finalWords = [];
 
-    const finalWords = [];
+  for (let l = 0; l < sorted.length; l++) {
+    finalWords.push({ name: sorted[l].slice("Word".length), pos: str.indexOf(sorted[l]) });
+  }
 
-    for (let l = 0; l < sorted.length; l++) {
-      finalWords.push({
-        name: sorted[l].slice("Word".length),
-        pos: str.indexOf(sorted[l]),
-      });
-    }
+  finalWords.sort(function (a, b) {
+    return a.pos - b.pos;
+  });
 
-    finalWords.sort(function (a, b) {
-      return a.pos - b.pos;
-    });
+  const pushedWords = [];
 
-    const pushedWords = [];
-
-    finalWords.forEach((obj) => {
-      pushedWords.push(obj.name);
-    });
-    //let wordsToPush = pushedWords
-    //.splice(0, 1)
-    //wordsToPush[0] == wordsToPush[0].substring(0, "Word".length)
-    // console.log(wordsToPush[0])
-    return pushedWords;
-  } catch {}
+  finalWords.forEach((obj) => {
+    pushedWords.push(obj.name);
+  });
+  //let wordsToPush = pushedWords
+  //.splice(0, 1)
+  //wordsToPush[0] == wordsToPush[0].substring(0, "Word".length)
+ // console.log(wordsToPush[0])
+  return pushedWords;
+  } catch {
+    
+  }
 }
 
-// @ts-ignore
-// @ts-ignore
 const readFile = new Promise((resolve, reject) => {
-  // @ts-ignore
-  // @ts-ignore
-  fs.readFile("Source.json", {}, function (err, predata) {
-    //console.log(predata.toString('utf-8'))
-    let pathlist = JSON.parse(predata.toString("utf-8"));
-    const paths = jsonpath.paths(pathlist, "$..*");
-    const data = paths.map((p) => p.join(".")).filter((p) => p != "");
-    function displayObj() {
-      // @ts-ignore
-      // @ts-ignore
-      let action = inquirer
-        .prompt([
-          {
-            type: "list",
-            name: "action",
-            message:
-              "Cull some of the snippets (select anything and choose a global option):",
-            choices: data,
-          },
-        ])
-        // @ts-ignore
-        // @ts-ignore
-        .then((answers) => {
-          inquirer
+fs.readFile("Source.json", {}, function(err, predata){
+  //console.log(predata.toString('utf-8'))
+  let pathlist = JSON.parse(predata.toString('utf-8'))
+  const paths = jsonpath.paths(pathlist, "$..*");
+  const data = paths.map((p) => p.join(".")).filter((p) => p != "");
+ function displayObj(){
+  let action = inquirer
+            .prompt([
+              {
+                type: "list",
+                name: "action",
+                message: "Cull some of the snippets (select anything and choose a global option):",
+                choices: data,
+              },
+            ])
+            .then((answers) => {
+              inquirer
             .prompt([
               {
                 type: "list",
                 name: "action",
                 message: "Choose a action:",
-                choices: ["tree veiw full", "cull layers"],
+                choices: ["tree veiw", "cull layers"],
               },
             ])
             .then((answers) => {
-              if (answers.action == "tree veiw full") {
-                let page = 0;
-                let ChoosenOptions = [];
-                function pageSelect(exclusiveOrInclusive){
-                let newpathlist = pathlist.$;
-                let slicedArray = null;
-                let shuffledArray = null;
-                if (exclusiveOrInclusive == false){
-                slicedArray = Object.keys(newpathlist).sort(
-                  () => Math.random() - 0.5
-                );
-                slicedArray = slicedArray.filter( function( e ) {
-                   return ChoosenOptions.indexOf( e ) < 0;
-                } );
-                // @ts-ignore
-                // @ts-ignore
-                 shuffledArray = slicedArray.slice(0, 10);
-              } else {
-                // @ts-ignore
-                shuffledArray = ChoosenOptions.slice(0, ChoosenOptions.length-10)
+              if (answers.action == "tree veiw"){
+              // @ts-ignore
+              console.log(treeify.asTree(pathlist, true))
               }
-                //console.log(shuffledArray.length)
-                let newObj = {};
-                // @ts-ignore
-                for (let i = 0; shuffledArray.length > i; i++) {
-                  //console.log(newpathlist.Object)
-                  // @ts-ignore
-                  newObj[shuffledArray[i]] = newpathlist[shuffledArray[i]];
-                }
-                console.log(
-                  // @ts-ignore
-                  treeify.asTree(newObj, true) + "\n" + `           page ${page}`
-                );
-                inquirer
-                  .prompt([
-                    {
-                      type: "list",
-                      name: "action",
-                      message: "Select a action",
-                      choices: ["back", "next"],
-                    },
-                  ])
-                  .then((answers) => {
-                    if (answers.action == "next"){
-                    ChoosenOptions = ChoosenOptions.concat(shuffledArray);
-                    page = page + 1;
-                    pageSelect(false);
-                    } else {
-                      page = page - 1
-                      pageSelect(true);
-                    }
-                  });
-                }
-                pageSelect(false)
-              }
-            });
-        });
-    }
-    displayObj();
-  });
+            })
+
+          })
+        }
+})
 
   //try {
-  //   const newRes = {};
-  //   let exportres = {};
+//   const newRes = {};
+//   let exportres = {};
 
-  //   fs.readFile("Source.json", "utf8", (err, data) => {
-  //     if (err) {
-  //       console.error(err);
-  //       return;
-  //     }
-  //     // console.log(data)
+//   fs.readFile("Source.json", "utf8", (err, data) => {
+//     if (err) {
+//       console.error(err);
+//       return;
+//     }
+//     // console.log(data)
 
-  //     const obj = JSON.parse(data).$;
+//     const obj = JSON.parse(data).$;
 
-  //     const paths = jsonpath.paths(obj, "$..*");
-  //     const pathList = paths.map((p) => p.join(".")).filter((p) => p != "");
-  // //console.log(lastChildPaths)
-  //     //console.log(pathList)
-  //     for (let i = 0; i < pathList.length; i++) {
-  //       try {
-  //         let path = pathList[i];
-  //         path = path.replace(/-/g, ".");
-  //         const res_obj = jsonpath.parse(path);
-  //         let prevPath = "";
-  //         for (let j = 0; j < res_obj.length; j++) {
+//     const paths = jsonpath.paths(obj, "$..*");
+//     const pathList = paths.map((p) => p.join(".")).filter((p) => p != "");
+// //console.log(lastChildPaths)
+//     //console.log(pathList)
+//     for (let i = 0; i < pathList.length; i++) {
+//       try {
+//         let path = pathList[i];
+//         path = path.replace(/-/g, ".");
+//         const res_obj = jsonpath.parse(path);
+//         let prevPath = "";
+//         for (let j = 0; j < res_obj.length; j++) {
 
-  //           //work here
-  //         }
-
-  //         //console.log(newRes)
-  //         resolve(newRes);
-  //       } catch (error) {
-  //         console.log(error);
-  //       }
-  //     }
-  //   });
-  //} catch (err){
+//           //work here
+//         }
+       
+//         //console.log(newRes)
+//         resolve(newRes);
+//       } catch (error) {
+//         console.log(error);
+//       }
+//     }
+//   });
+//} catch (err){
   //if (err.substring(0, "TypeError: Cannot read properties of undefined (reading '0')".length) != "TypeError: Cannot read properties of undefined (reading '0')"){
-  //console.error(err)
+    //console.error(err)
   //}
-  //}
+//}
 });
 
 let currentkeys = [];
@@ -204,11 +134,7 @@ let selectionPath = "";
 let selected = [];
 
 let finalKeys = {};
-// @ts-ignore
-// @ts-ignore
 let a = {};
-// @ts-ignore
-// @ts-ignore
 let b = {};
 
 //let allInvoked = {};
@@ -221,13 +147,13 @@ let b = {};
 readFile.then((todata) => {
   const jsonString = JSON.stringify(todata, null, 2);
 
-  fs.writeFile("Source2.json", jsonString, "utf8", function (err) {
-    if (err) {
-      console.error("Error writing JSON to file:", err);
-    } else {
-      // console.log('JSON has been written to file successfully.');
-    }
-  });
+fs.writeFile('Source2.json', jsonString, 'utf8', function (err) {
+  if (err) {
+    console.error('Error writing JSON to file:', err);
+  } else {
+   // console.log('JSON has been written to file successfully.');
+  }
+});
   function overrun(seedata) {
     function objselect(obj) {
       return new Promise(function (resolve, reject) {
@@ -242,27 +168,21 @@ readFile.then((todata) => {
             currentkeys.push(Object.keys(obj)[i]);
             delete finalKeys[Object.keys(obj)[i]];
           }
-
-          fs.writeFile(
-            "finalObj.json",
-            JSON.stringify(finalKeys, null, 2),
-            // @ts-ignore
-            // @ts-ignore
-            function (error) {}
-          );
+          
+            fs.writeFile(
+                "finalObj.json",
+                JSON.stringify(finalKeys, null, 2),
+                function (error) {}
+            );
         }
 
-        // @ts-ignore
-        // @ts-ignore
         let user = inquirer
           .prompt([
             {
               type: "checkbox",
               name: "selectedItem",
               message: "Select an item:",
-              choices: currentkeys.filter(
-                (e) => e !== "name" && e !== "prefix"
-              ),
+              choices: currentkeys.filter(e => e !== 'name' && e !== 'prefix')
             },
           ])
           .then((answers) => {
@@ -298,14 +218,10 @@ readFile.then((todata) => {
     let current_obj = seedata;
     async function Run() {
       while (current_obj instanceof Object) {
-        // @ts-ignore
-        // @ts-ignore
         await new Promise(function (resolve, reject) {
           function resolution() {
             resolve();
           }
-          // @ts-ignore
-          // @ts-ignore
           let action = inquirer
             .prompt([
               {
@@ -326,8 +242,6 @@ readFile.then((todata) => {
               if (answers.action === "Select") {
                 // finalKeys = current_obj
                 objselect(current_obj)
-                  // @ts-ignore
-                  // @ts-ignore
                   .then((answer) => {
                     return resolution();
                   })
@@ -341,8 +255,6 @@ readFile.then((todata) => {
                 resolution();
               } else if (answers.action === "Select and view") {
                 objselect(current_obj)
-                  // @ts-ignore
-                  // @ts-ignore
                   .then((answer) => {
                     return resolution();
                   })
@@ -354,19 +266,18 @@ readFile.then((todata) => {
                 current_obj = current_obj[selectionPath];
               } else if (answers.action === "Back") {
                 // let jsonToPath = {...todata}
-                let findPath = jsonpath
-                  .paths(todata, `$..[?(@.name == '${selectionPath}')]`)[0]
-                  .slice(0, -2);
-
-                const parentPath = findPath;
-                const nodes = jsonpath.nodes(
+                let findPath = jsonpath.paths(
                   todata,
-                  jsonpath.stringify(parentPath)
-                );
+                  `$..[?(@.name == '${selectionPath}')]`
+                )[0].slice(0, -2)
+               
+                const parentPath = findPath;
+                const nodes = jsonpath.nodes(todata, jsonpath.stringify(parentPath));
                 current_obj = nodes[0].value;
                 resolution();
               } else if (answers.action === "Finished") {
-              }
+
+              } 
             });
         });
       }
